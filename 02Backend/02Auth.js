0992a8346f5c189c7f215e615078962541c882dd
Vikash Kumar <vk2542883@gmail.com>
2026-06-28 10:55:29 +0530

    Add files via upload

---
+++ 02Backend/02Auth.js
@@ -0,0 +1,9 @@
+let login=true;
+console.log(login);
+let dipesh="good student";
+
+function Auth(){
+    console.log("login successful")
+}
+
+module.exports={login,Auth};
---
+++ 02Backend/02main.js
@@ -0,0 +1,7 @@
+console.log("hi running")
+// let featureX=require("./Auth")
+// console.log(featureX);
+// featureX.Auth();
+let {login,Auth}=require("./02Auth.js");
+Auth();
+console.log(login)
