ce1ce41cdbbc670a168ad4c1b518f8eae2ef96b4
Vikash Kumar <vk2542883@gmail.com>
2026-06-28 11:18:29 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose=require("mongoose");
 
 const connectDB= async ()=>{
-    try{
-        mongoose.connect("mongodb://localhost:27017/Product")
-        console.log("DB Connected");        
-    }
-    catch(err){
-        console.log(err);       
-    }
-};
-module.exports= connectDB;
+    mongoose.connect("mongodb://127.0.0.1:27017/Student_Database")
+    .then(()=>{
+        console.log("DB Connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+module.exports=connectDB;
